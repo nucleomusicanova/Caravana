fcc9b128acd4b05bfd56ae81028b49958eda58b1
Charles K. Neimog <charlesneimog@outlook.com>
2024-08-23 22:16:07 -0300

    updates

--- patch1.js
+++ patch1.js
@@ -1,60 +1,145 @@
+var mouseOrTouchX = 0;
+var mouseOrTouchY = 0;
+
 class Patch1 {
   constructor() {
     this.rects = [];
+    this.hackRects = [];
+    this.imageRects = [];
     this.circles = [];
     this.lines = [];
     this.processes = [];
+    this.colors = [];
+    this.someIsSelected = false;
   }
 
-  addRect(x, y, w, h, scale, id) {
-    this.rects.push({ x: x, y: y, w: w, h: h, scale: scale, inside: false, id: id, defaultX: x, defaultY: y });
+  addRect(x, y, w, h, scale, color, id) {
+    this.rects.push({
+      x: x,
+      y: y,
+      w: w,
+      h: h,
+      scale: scale,
+      inside: false,
+      id: id,
+      defaultX: x,
+      defaultY: y,
+      draw: false,
+      color: color,
+    });
   }
 
-  addCircle(x, y, r, id) {
-    this.circles.push({ x: x, y: y, r: r, inside: false, id: id, defaultX: x, defaultY: y });
+  addImageRect(image, x, y, scale, id) {
+    this.imageRects.push({
+      image: image,
+      x: x,
+      y: y,
+      w: image.width,
+      h: image.height,
+      scale: scale,
+      id: id,
+    });
   }
 
-  addProcess(image, x, y, scale, id) {
-    this.processes.push({ image: image, x, y, scale, id, defaultY: y, defaultX: x });
-    console.log(this.processes);
+  addHackRect(x, y, w, h, scale, label, id) {
+    this.hackRects.push({ x: x, y: y, w: w, h: h, label: label, id: id, draw: false });
+  }
+
+  addCircle(x, y, r, color, id) {
+    this.circles.push({
+      x: x,
+      y: y,
+      r: r,
+      inside: false,
+      id: id,
+      defaultX: x,
+      defaultY: y,
+      draw: false,
+      color: color,
+    });
+  }
+
+  addProcess(image, x, y, scale, color, id) {
+    this.processes.push({ image: image, x, y, scale, color, id, defaultY: y, defaultX: x });
+    // console.log(this.processes);
   }
 
   addLine(thing1, thing2) {
     this.lines.push({ thing1, thing2 });
   }
 
+  setColor(id, color) {
+    this.colors.push({ id, color });
+    // console.log(this.colors);
+  }
+
+  getColor(id) {
+    var color = this.colors.find((color) => color.id === id);
+    // console.log(color
+    return color;
+  }
+
+  drawHackRects() {
+    this.hackRects.forEach((thisRect) => {
+      noStroke();
+      fill(fColor.r, fColor.g, fColor.b);
+      rect(thisRect.x, thisRect.y, thisRect.w, thisRect.h, 3, 3);
+      fill(fColor.r, fColor.g, fColor.b);
+      textSize(12);
+      text(thisRect.label, thisRect.x + 13, thisRect.y + 65);
+    });
+  }
+
+  drawImageRects() {
+    this.imageRects.forEach((thisImage) => {
+      image(thisImage.image, thisImage.x, thisImage.y, thisImage.w * thisImage.scale, thisImage.h * thisImage.scale);
+    });
+  }
+
   drawRects(process) {
     this.rects.forEach((thisRect) => {
+      thisRect.inside = true;
       if (process.x > thisRect.x && process.x < thisRect.x + thisRect.w && process.y > thisRect.y && process.y < thisRect.y + thisRect.h) {
-        fill(255, 0, 0);
-        rect(thisRect.x, thisRect.y, thisRect.w, thisRect.h);
         if (!mouseIsPressed) {
           process.x = thisRect.x + thisRect.w / 2 - (process.image.width * process.scale) / 2;
           process.y = thisRect.y + thisRect.h / 2 - (process.image.height * process.scale) / 2;
         }
         process.inside = true;
-      } else {
-        fill(255);
-        rect(thisRect.x, thisRect.y, thisRect.w, thisRect.h);
+        thisRect.inside = true;
+        if (process.house === null) {
+          process.house = thisRect;
+        }
       }
+      if (process.house && process.house.id === thisRect.id) {
+        fill(thisRect.color.r, thisRect.color.g, thisRect.color.b);
+      } else {
+        fill(fColor.r, fColor.g, fColor.b);
+      }
+      rect(thisRect.x, thisRect.y, thisRect.w, thisRect.h, 3, 3);
+      thisRect.color = fColor;
     });
   }
 
   drawCircles(process) {
     this.circles.forEach((thisCircle) => {
-      fill(255);
-      circle(thisCircle.x, thisCircle.y, thisCircle.r);
       if (dist(process.x, process.y, thisCircle.x, thisCircle.y) < thisCircle.r) {
-        fill(255, 0, 0);
-        circle(thisCircle.x, thisCircle.y, thisCircle.r);
         process.inside = true;
         if (!mouseIsPressed) {
           process.x = thisCircle.x - (process.image.width * process.scale) / 2;
           process.y = thisCircle.y - (process.image.height * process.scale) / 2;
         }
-      } else {
-        fill(255);
+        process.inside = true;
+        if (process.house === null) {
+          process.house = thisCircle;
+        }
       }
+      if (process.house && process.house.id === thisCircle.id) {
+        fill(thisCircle.color.r, thisCircle.color.g, thisCircle.color.b);
+      } else {
+        fill(fColor.r, fColor.g, fColor.b);
+      }
+      circle(thisCircle.x, thisCircle.y, thisCircle.r);
+      thisCircle.color = fColor;
     });
   }
 
@@ -63,9 +148,16 @@ class Patch1 {
     if (!thing1) {
       thing1 = this.circles.find((circle) => circle.id === thing1Id);
     }
+    if (!thing1) {
+      thing1 = this.imageRects.find((imageRect) => imageRect.id === thing1Id);
+    }
+
     var thing2 = this.rects.find((rect) => rect.id === thing2Id);
     if (!thing2) {
       thing2 = this.circles.find((circle) => circle.id === thing2Id);
+    }
+    if (!thing2) {
+      thing2 = this.imageRects.find((imageRect) => imageRect.id === thing2Id);
     }
 
     if (!thing1 || !thing2) {
@@ -89,15 +181,25 @@ class Patch1 {
     var thing1IsBottom = thing1.y > thing2.y;
 
     if (thing1IsSameX && thing1IsRect && thing2IsRect) {
-      stroke(0);
+      stroke(255);
       line(thing1.x + thing1.w / 2, thing1.y + thing1.h, thing2.x + thing2.w / 2, thing2.y);
       return;
     }
 
+    if (thing1IsCircle && !thing2IsCircle && thing1IsLeft) {
+      stroke(255);
+      line(thing1.x + thing1.r, thing1.y, thing2.x, thing2.y + thing2.h / 2);
+      return;
+    }
+
     if (thing1IsLeft && thing1IsRect && thing2IsCircle) {
-      fill(0);
-      stroke(0);
+      fill(bColor.r, bColor.g, bColor.b);
+      stroke(255);
       line(thing1.x + thing1.w, thing1.y + thing1.h / 2, thing2.x - thing2.r / 2, thing2.y);
+    }
+
+    if (thing1IsRight && !thing1IsRect && thing2IsRect) {
+      line(thing1.x + thing1.r / 2, thing1.y, thing2.x, thing2.y + (thing2.h / 2) * thing2.scale);
     }
   }
 
@@ -110,30 +212,80 @@ class Patch1 {
         mouseY < process.y + process.image.height * process.scale
       ) {
         cursor(HAND);
-        if (mouseIsPressed) {
-          process.x = mouseX;
-          process.y = mouseY;
+        if (mouseIsPressed && !this.someIsSelected) {
+          process.selected = true;
+        } else {
+          process.selected = false;
         }
       }
+    });
+    this.processes.forEach((process) => {
       if (!process.inside && !mouseIsPressed) {
         process.x = process.defaultX;
         process.y = process.defaultY;
+      }
+
+      if (process.house) {
+        // if (!mouseIsPressed) {
+        fill(process.color.r, process.color.g, process.color.b);
+        // }
+        if (process.house.r) {
+          process.draw = true;
+          circle(process.house.x, process.house.y, process.house.r);
+          process.house.process = process;
+        } else {
+          rect(process.house.x, process.house.y, process.house.w, process.house.h, 3, 3);
+          process.house.process = process;
+        }
       }
       image(process.image, process.x, process.y, process.image.width * process.scale, process.image.height * process.scale);
     });
   }
 
   draw() {
-    // check if mouse is inside processes
+    if (!mouseIsPressed) {
+      this.someIsSelected = false;
+      this.selectedId = "";
+    }
     this.processes.forEach((process) => {
+      if (
+        mouseX > process.x - process.image.width * process.scale &&
+        mouseX < process.x + process.image.width * process.scale &&
+        mouseY > process.y - process.image.height * process.scale &&
+        mouseY < process.y + process.image.height * process.scale
+      ) {
+        cursor(HAND);
+        process.house = null;
+        if (mouseIsPressed && !this.someIsSelected) {
+          process.selected = true;
+          this.someIsSelected = true;
+          this.selectedId = process.id;
+          process.x = mouseX;
+          process.y = mouseY;
+        } else if (mouseIsPressed && this.selectedId === process.id) {
+          process.x = mouseX;
+          process.y = mouseY;
+        } else {
+          process.selected = false;
+        }
+      } else {
+        process.selected = false;
+      }
       process.inside = false;
+
       this.drawRects(process);
       this.drawCircles(process);
+    });
+
+    this.hackRects.forEach((hackRect) => {
+      this.drawHackRects(hackRect);
     });
 
     this.lines.forEach((line) => {
       this.drawLines(line.thing1, line.thing2);
     });
+
+    this.drawImageRects();
     this.drawProcesses();
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,45 +1,101 @@
-
 // Patch
 var patch = new Patch1();
 
-
-
 // Background color
-const backgroundColor = 245;
+// const backgroundColor = 250;
+const bColor = { r: 48, g: 48, b: 48 };
+const fColor = { r: 233, g: 233, b: 233 };
 
+var Synth1;
+var Synth2;
+var Sound;
 
 function preload() {
-  Synth = loadImage("svg/synth.svg");
+  Synth1 = loadImage("svg/synth.svg");
+  Synth2 = loadImage("svg/synth.svg");
+  Sound = loadImage("svg/sound.svg");
 }
 
 function setup() {
-  let myCanvas = createCanvas(400, 500);
+  let myCanvas;
+  var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  if (isMobile) {
+    // get the width of the device
+    var width = window.innerWidth - window.innerWidth * 0.1;
+    myCanvas = createCanvas(width, width * 1.25);
+  } else {
+    myCanvas = createCanvas(500, 600);
+  }
   myCanvas.parent("mySketch");
-  background(backgroundColor);
+  background(bColor.r, bColor.g, bColor.b);
   pixelDensity(1);
   imageMode(CORNER);
-  
 
   // Patch1
-  patch.addRect(10, 150, 100, 100, 1, "rect1");
-  patch.addRect(10, 300, 100, 100, 1, "rect2");
-  patch.addCircle(200, 350, 100, "circle1");
-  patch.addProcess(Synth, 10, 10, 0.1, "synth");
+  patch.addRect(10, 150, 100, 100, 1, fColor, "rect1");
+  patch.addRect(10, 300, 100, 100, 1, fColor, "rect2");
+  patch.addCircle(200, 350, 100, fColor, "circle1");
+  patch.addImageRect(Sound, 300, 300, 0.125, "dac");
+
+  // Synth
+  patch.addHackRect(10, 10, 86, 50, 1, "MidiSynth", "defaultSynth");
+  patch.addProcess(Synth1, 13, 13, 0.1, { r: 155, g: 0, b: 0 }, "synth");
+
+  // Synth2
+  patch.addHackRect(110, 10, 86, 50, 1, "MidiSynth2", "defaultSynth2");
+  patch.addProcess(Synth2, 113, 13, 0.1, { r: 0, g: 0, b: 155 }, "synth2");
+
+  // Synth3
+  patch.addHackRect(220, 10, 86, 50, 1, "MidiSynth3", "defaultSynth3");
+  patch.addProcess(Synth2, 223, 13, 0.1, { r: 0, g: 155, b: 0 }, "synth3");
 
   // Draw Lines
   patch.addLine("rect1", "rect2");
   patch.addLine("rect2", "circle1");
-
-
-
+  patch.addLine("circle1", "dac");
 }
 
 // ===============================
 function draw() {
   cursor(ARROW);
-
-  background(backgroundColor);
+  background(bColor.r, bColor.g, bColor.b);
   patch.draw();
-
 }
 
+function doubleClicked() {
+  // patch.doubleClicked();
+}
+
+// function touchStarted() {
+//   // Check if touch is within the circle
+//   if (dist(touchX, touchY, circleX, circleY) < circleSize / 2) {
+//     dragging = true;
+//     // Calculate offset to maintain relative position while dragging
+//     offsetX = circleX - touchX;
+//     offsetY = circleY - touchY;
+//   }
+// }
+
+// // Touch moved
+// function touchMoved() {
+//   // Update circle position if dragging
+//   if (dragging) {
+//     circleX = touchX + offsetX;
+//     circleY = touchY + offsetY;
+//     mouseX = touchX;
+//     mouseY = touchY;
+//   }
+//   return false; // Prevents default behavior
+// }
+
+// function touchEnded() {
+//   // Stop dragging when touch ends
+//   dragging = false;
+// }
+
+// if p is pressed, then save the canvas as a png
+function keyPressed() {
+  if (key === "p") {
+    saveCanvas("patch1", "png");
+  }
+}
